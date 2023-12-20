12e3dc3613f4b5f42409186b7ecb87af167b1b0e
okxooxoo <okxooxoo@naver.com>
2023-12-21 04:00:07 +0900

    ✨ feat: 웹 소켓 서버 생성 및 브라우저로부터 메시지 전송받기

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,7 +9,26 @@ app.set("views", __dirname + "/views"); // pug 파일을 담을 views 폴더 위
 app.use("/public", express.static(__dirname + "/public"));
 
 const handleListen = () => console.log("3000 포트에서 실행 중...");
-app.listen(3000, handleListen);
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    });
+});
+
+server.listen(3000, handleListen);
