f555e8b5d7e3c513d2616d70c5287dbb25e42ce6
okxooxoo <okxooxoo@naver.com>
2023-12-23 19:12:32 +0900

    ✨ feat: 닉네임 설정 후 채팅 기능 구현

--- src/server.js
+++ src/server.js
@@ -7,28 +7,38 @@ const app = express();
 app.set("view engine", "pug"); // pug 템플릿 엔진 사용
 app.set("views", __dirname + "/views"); // pug 파일을 담을 views 폴더 위치 알려주기
 app.use("/public", express.static(__dirname + "/public"));
-
-const handleListen = () => console.log("3000 포트에서 실행 중...");
-
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("3000 포트에서 실행 중...");
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
+  console.log("Disconnected from the Browser ❌");
 }
 
 const sockets = [];
 
 wss.on("connection", (socket) => {
-    sockets.push(socket);
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()));
-    });
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
 });
 
 server.listen(3000, handleListen);
